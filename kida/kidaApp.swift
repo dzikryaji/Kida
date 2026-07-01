2d045cefdc5c41bbcd318c885086f5b7f5640b1e
Dzikry Aji Santoso <100667379+dzikryaji@users.noreply.github.com>
2026-07-01 09:09:29 +0800

    Initial Commit

---
+++ kida/kidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidaApp.swift
+//  kida
+//
+//  Created by Dzikry Aji Santoso on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct kidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
